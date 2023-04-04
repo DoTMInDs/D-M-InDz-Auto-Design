ec491f047a5c6cee1646c6d309f636991fd22f7b
D•M•InDz <123625827+DoTMInDs@users.noreply.github.com>
2023-04-04 18:10:59 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,102 +1,38 @@
-var menulist = document.getElementById('menulist');
+const list = document.getElementById('list')
+// const user = document.getElementById('user')
 
 function openmenu(){
-    menulist.style.right = '0'
+    list.style.right = '0'
+    // user.style.display = 'block'
 }
-
 function closemenu(){
-    menulist.style.right = '-200px'
-}
-
-function openlog(){
-    window.location = 'login.html';
-}
-function opensign(){
-    window.location = 'Sign-up.html';
-}
-
-function goToHome(){
-    window.location = 'index.html';
+    list.style.right = '-200px'
+    // user.style.display = 'none'
 }
 
 
-// window.location.href = "";
 
 function SubmitEvent(){
-    var firstname = document.getElementById('firstname');
-    var lastname = document.getElementById('lastname');
     var email = document.getElementById('email');
     var password = document.getElementById('password');
-    var confirmpassword = document.getElementById('conf_password');
-    // var message = document.getElementById('message').value;
-   
+    var form = document.getElementById('login-form')
 
-    if(firstname.value == "" || lastname.value == "" || email.value == "" || password.value == "" || confirmpassword.value == ""){
+    if(email.value == "" || password.value == ""){
+        alert('invalid information');   
+    }
+    else{
         confirm('Please click ok to confirm...')
-        alert('invalid information');
-        return false;
-    }
-    else {
-        alert('login Successfully');
-    }
-}
-
-//     Form.addEventListener('submit', function (e) {
-//     e.preventDefault();
-// })
-    // var email = document.getElementById('email');
-    // var phone = document.getElementById('phone');
-    // var make = document.getElementById('make');
-    // var model = document.getElementById('model');
-    // var message = document.getElementById('message');
-
-function sendEvent(){
-
-    var email = document.getElementById('email');
-    var name = document.getElementById('name');
-    var phone = document.getElementById('phone');
-    var make = document.getElementById('make');
-    var model = document.getElementById('model');
-    
-    if (name.value == "" || phone.value == "" || email.value == "" || make.value == "" || model.value == ""){
-        
-        alert('invalid information');
-
-        name.focus();
-        email.focus();
-        phone.focus();
-        make.focus();
-        model.focus();
-
-        email.style.border = "solid 3px red";
-        name.style.border = "solid 3px red";
-        phone.style.border = "solid 3px red";
-        make.style.border = "solid 3px red";
-        model.style.border = "solid 3px red";
-        return false;
-    }
-    
-    else{
-        alert('Message Sent. Thank you!')
+        alert('Submitted Succesfully. Thank you!')
         return;
     }
-    
 }
 
-function myFunction(){
-    var required = document.getElementById('required');
-    
-
-    if (required.value == ""){
-        alert('Enter your email');
-        return;
-    }
-    else{
-        alert('please check your email to reset password. Thank you!');
-        window.location = "index.html";
-    }
+function goHome(){
+    window.location = 'index.html';
 }
-
-function goToReset(){
-    window.location = "forgotpassword.html";
+function goToLogin(){
+    window.location = 'login.html';
+}
+function goToSignin(){
+    window.location = 'register.html';
 }
